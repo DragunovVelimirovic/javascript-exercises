0b3c01ac23005831e73154c8ca8fe14ab0d28738
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-02-28 22:27:13 -0400

    Fourth exercise completed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    
+    const argArray = args[0];
+    let outputArray = [];
+    
+    argArray.forEach((item) => {
 
+        if (!args.includes(item)) {
+            outputArray.push(item);
+        }
+    });
+
+    return outputArray;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
