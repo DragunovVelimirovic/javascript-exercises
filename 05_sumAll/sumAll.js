c71b8c06d3dcf08c638fa652f2186de8b3a975c8
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-02-28 22:52:43 -0400

    Fifth exercise completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(inputOne, inputTwo) {
 
+    let sum = 0;
+    let smallerNum = inputOne;
+    let largerNum = inputTwo;
+
+    if (inputOne > inputTwo) {
+        
+        let swapHolder = smallerNum;
+        smallerNum = largerNum;
+        largerNum = swapHolder;
+
+    }
+
+    for (let nextNum = smallerNum; nextNum <= largerNum; nextNum++) {
+        
+        sum += nextNum;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
