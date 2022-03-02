87d1f969313507ea536060e56170613456f50608
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-03-02 11:30:50 -0400

    Seventh exercise completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,17 @@
-const ftoc = function() {
+const ftoc = function(inputFahrenheit) {
 
+    let outputCelsius = 0;
+
+    outputCelsius = (inputFahrenheit - 32) * (5/9);
+    return Math.round(outputCelsius * 10) /10;
 };
 
-const ctof = function() {
+const ctof = function(inputCelsius) {
 
+    let outputFahrenheit = 0;
+
+    outputFahrenheit = 1.8 * inputCelsius + 32;
+    return Math.round(outputFahrenheit * 10) /10;  
 };
 
 // Do not edit below this line
