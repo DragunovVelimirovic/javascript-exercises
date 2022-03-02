cfbbacdbe5cbb93ba5ce13c3d86dfff05a5c9e2e
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-03-02 10:55:54 -0400

    Sixth exercise completed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,30 @@
-const leapYears = function() {
+const leapYears = function(inputYear) {
 
+    let yearCounter = 0;
+
+    if (inputYear % 4 === 0){
+
+        yearCounter ++;
+    }
+    
+    if (inputYear % 100 === 0) {
+        
+        yearCounter --;
+    }
+
+    if (inputYear % 400 === 0) {
+        
+        yearCounter ++;
+    }
+
+    if (yearCounter >= 1) {
+        
+        return true;
+    }
+    else {
+
+        return false;
+    }
 };
 
 // Do not edit below this line
