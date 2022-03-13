0d01e0db46fb3b063a988d8953ea566107f836ef
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-03-13 18:00:07 -0300

    calculator exercise finished

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(x, y) {
+    return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+    return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(inputArray) {
+    let sum = 0;
+    inputArray.forEach(num => {
+        sum += num;
+    });
+    return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(inputArray) {
+    let product = 1;
+    inputArray.forEach(num => {
+        product = product * num;
+    });
+    return product;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	let numToPower = Math.pow(x, y);
+    return numToPower;
 };
 
-const factorial = function() {
-	
+const factorial = function(inputNum) {
+    if (inputNum === 0) {
+        return 1;
+    }
+
+	let numFactorialed = inputNum;
+    for (let i = 1; i < inputNum; i++) {
+        numFactorialed = numFactorialed * (inputNum - i);
+    }
+    return numFactorialed;
 };
 
 // Do not edit below this line
