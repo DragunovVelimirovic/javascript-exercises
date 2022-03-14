7f93a40b7433392ebed6571ce2a664ac63002699
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-03-14 11:58:19 -0300

    Palindrones exercise completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (inputArray) {
+    let splitArray = inputArray.split("");
+    splitArray = splitArray.filter(char => (/[a-zA-Z]/).test(char)); 
 
+    // Creating an array back into a string with punctuation removed to compare with the reversed string with punctuation removed
+    let punctuationRemoved = splitArray;
+    punctuationRemoved = punctuationRemoved.join("");
+
+    let reverseArray = splitArray.reverse();
+    let reversedString = reverseArray.join("");
+
+    if (reversedString.toLowerCase() === punctuationRemoved.toLowerCase()) {
+        return true;
+    }
+    else {
+        return false;
+    }
 };
 
 // Do not edit below this line
