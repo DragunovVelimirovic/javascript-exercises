51d98b0c01bb0f0fe12e5d0f78fa003738817791
DragunovVelimirovic <blake_v1993@hotmail.com>
2022-03-14 12:47:35 -0300

    Fibonacci exercise completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(inputNum) {
+    if (inputNum < 0) {
+        return "OOPS";
+    }
+    
+    let prevNum = 0;
+    let currentNum = 1;
 
+    for (let i = 0; i < inputNum; i++) {
+        currentNum += prevNum;
+        prevNum = currentNum - prevNum;
+        
+        // If it's only the first loop, reset prevNum to zero to signify there not being a previous number
+        if (i === 0) {
+            prevNum = 0;
+        }
+    }
+    return currentNum;
 };
 
 // Do not edit below this line
